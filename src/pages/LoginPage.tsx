23592c3b5c143166b22f5969fc544d6f1f7f967f
Andriy Kashyrskyy <andriy@Andriys-MacBook-Air.local>
2025-06-04 16:33:35 -0400

    Added improved view and functionality for Login page.

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,21 +1,29 @@
+// src/pages/LoginPage.tsx
 import { signInWithPopup } from 'firebase/auth';
 import { auth, googleProvider } from '../firebase/firebase';
 import { useUserStore } from '../store/useUserStore';
 import { useNavigate } from 'react-router-dom';
 
+import {
+  Box,
+  Button,
+  Card,
+  CardContent,
+  Typography,
+  Stack,
+  Avatar,
+} from '@mui/material';
+import GoogleIcon from '@mui/icons-material/Google';
+
 function LoginPage() {
   const setUser = useUserStore((state) => state.setUser);
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
   const handleLogin = async () => {
     try {
       const result = await signInWithPopup(auth, googleProvider);
       const user = result.user;
-      setUser({
-        uid: user.uid,
-      });
-      
-      // Redirect to home page
+      setUser({ uid: user.uid });
       navigate('/home');
     } catch (err) {
       console.error('Login error:', err);
@@ -23,10 +31,47 @@ function LoginPage() {
   };
 
   return (
-    <div>
-      <h1>Login</h1>
-      <button onClick={handleLogin}>Sign in with Google</button>
-    </div>
+    <Box
+      sx={{
+        minHeight: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        bgcolor: 'background.default',
+      }}
+    >
+      <Card sx={{ width: 360, p: 2, boxShadow: 3 }}>
+        <CardContent>
+          <Stack spacing={3} alignItems="center">
+            <Avatar
+              alt="iTEST"
+              sx={{ bgcolor: 'primary.main', width: 56, height: 56 }}
+            >
+              I
+            </Avatar>
+            <Typography variant="h5" fontWeight={600}>
+              iTEST DS Dashboard
+            </Typography>
+            <Button
+              variant="contained"
+              startIcon={<GoogleIcon />}
+              onClick={handleLogin}
+              sx={{
+                bgcolor: '#DB4437',
+                '&:hover': {
+                  bgcolor: '#c1351d',
+                },
+                textTransform: 'none',
+                fontWeight: 600,
+              }}
+              fullWidth
+            >
+              Sign in with Google
+            </Button>
+          </Stack>
+        </CardContent>
+      </Card>
+    </Box>
   );
 }
 
